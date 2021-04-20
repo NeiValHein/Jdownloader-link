aee7a01e577b60c5ddd255ecdcce70293576877a
NeiValHein <37918247+NeiValHein@users.noreply.github.com>
2021-04-20 11:38:00 -0600

    Match to all sites :)

--- userscript-decryptor.js
+++ userscript-decryptor.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        Decryptor
+// @name        Decryptor UserScript
 // @namespace   NeiValHein Scripts
-// @match       https://website.com/ // if you want to execute the userscript in other site then delete th current link and put the new website link
-// @grant       none
+// @include        http://*
+// @include        https://*
+// @connect        *
 // @version     1.0
 // @author      NeiValHein
-// @description 12/12/2020 09:06:08
+// @description Obtain encrypted link with this userscript and put them in Jdownloader Decryptor (see my GitHub profile to download)
  var seeHidden = document.evaluate("//input[@name='crypted']",    
  document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);    
  for (var i = seeHidden.snapshotLength - 1; i >= 0; i--) {   
